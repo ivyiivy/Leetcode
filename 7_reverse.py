4e117231f04aac7d8eacfddf5e5dd9ae90b59d27
ivy <1308670017@qq.com>
2019-09-20 16:11:00 +0800

    finish

--- 7_reverse.py
+++ 7_reverse.py
@@ -4,4 +4,20 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        
+        y = abs(x)
+        rx = 0
+        while y / 10:
+            rx = rx * 10 + y % 10
+            y = y // 10
+
+        if x < 0:
+            rx = rx * -1
+        if rx < -2 ** 31 or rx > 2 ** 31 - 1:
+            return 0
+        else:
+            return rx
+
+
+solution = Solution()
+print(solution.reverse(123))
+print(abs(2 ** 31))
